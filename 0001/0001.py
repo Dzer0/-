ed93a66dc4823a5a693ccf9315ea3ccc5f466eb6
Dzer0 <23442435@qq.com>
2016-02-17 22:50:21 +0800

    根据网上的练习题 完成了0000题 (cherry picked from commit aff87f216fbf691c3701e179613215bff8e9fb5c) 开始第二题 完成第二题

--- 0001/0001.py
+++ 0001/0001.py
@@ -1,1 +1,34 @@
 # coding:utf-8
+import random
+import string
+
+# zfc = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-"
+# zfc1 = ['z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e',
+# 		'd', 'c', 'b', 'a']
+#
+# s = []
+# for i in range(200):
+# 	for q in range(20):
+# 		s.append(random.choice(zfc))
+# 	yhm = ''.join(s)
+#
+# #print yhm
+# print string.ascii_letters
+# print string.digits
+
+# 一下是列表代码
+mlc = []
+for a in range(200):
+	mlc.append(''.join(random.sample((string.ascii_letters + string.digits), 20)))
+
+print mlc
+
+# 下面是字典代码
+two = {}
+a = 0
+for m in range(200):
+	a = a + 1
+	m = ''.join(random.sample((string.ascii_letters + string.digits), 20))
+	two[a] = m
+
+print two
