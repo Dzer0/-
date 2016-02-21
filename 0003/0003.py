8ce39d7d1d6dbe4f8d060cbfbe8710fe9d0414cc
Dzer0 <23442435@qq.com>
2016-02-21 19:58:16 +0800

    完成第三题. 完成

--- 0003/0003.py
+++ 0003/0003.py
@@ -9,3 +9,10 @@ for a in range(200):  # 循环200次
 print mlc[0]
 
 r = redis.Redis('localhost', 6379, 0)
+i = 0
+for i in range(200):
+	r.lpush('Mykey01', mlc[i])
+	i = i + 1
+
+print r.lrange('Mykey01', 0, -1)
+r.save()
